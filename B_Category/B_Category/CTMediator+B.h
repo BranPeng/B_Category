7f4e9b03de03a1609cb813066f36c24e3a9f6fa2
Bran <Bran.peng@yuanhengtech.cn>
2019-04-01 14:31:23 +0800

    spec

---
+++ B_Category/B_Category/CTMediator+B.h
@@ -0,0 +1,19 @@
+//
+//  CTMediator+B.h
+//  B_Category
+//
+//  Created by Binfeng Peng - Vendor on 2019/4/1.
+//  Copyright © 2019年 Binfeng Peng - Vendor. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (B)
+
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText;
+
+@end
+
+NS_ASSUME_NONNULL_END
